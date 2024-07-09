09b44a0410470af978c72707b826decae3f3e794
INGENIEROTOM <115744071+ptpachecop@users.noreply.github.com>
2024-07-09 17:28:32 -0500

    se agrega el proyecto

---
+++ products-example.js
@@ -0,0 +1,44 @@
+const data = {
+  _lastID: 6,
+  products: [
+    {
+      _ID: 1,
+      votes: {
+        1: 1,
+        2: 0,
+        3: 1,
+        4: 0,
+        5: 1,
+      },
+      cantStar: "3.0",
+      name: "Shoes sport",
+      price: "65",
+      image:
+        "https://img.freepik.com/psd-premium/nueva-platilla-banner-web-publicacion-feed-instagram-redes-sociales-exclusivas-zapatillas-correr_484627-193.jpg?w=740",
+      description:
+        "Los zapatos deportivos son un tipo de calzado diseñado para actividades físicas y deportivas. Están confeccionados con materiales cómodos y transpirables, lo que los hace ideales para lograr agilidad y comodidad durante el ejercicio.",
+    },
+    {
+      _ID: 2,
+      votes: {
+        1: 1,
+        2: 1,
+        3: 1,
+        4: 0,
+        5: 2,
+      },
+      cantStar: "3.2",
+      name: "Sneakers shoes",
+      price: "50",
+      image:
+        "https://img.freepik.com/psd-premium/venta-calzado-deportivo-publicacion-instagram-redes-sociales-o-diseno-plantilla-banner-cuadrado_70055-902.jpg",
+      description:
+        "Los zapatos deportivos son un tipo de calzado diseñado para actividades físicas y deportivas. Están confeccionados con materiales cómodos y transpirables, lo que los hace ideales para lograr agilidad y comodidad durante el ejercicio.",
+    },
+   
+    
+    
+  ],
+};
+
+export default data;
